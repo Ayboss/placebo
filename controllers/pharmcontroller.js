0ba8d17b03617a340ecc047292457824014e3a07
Ayobami Lawal <bamiayo90@gmail.com>
2024-05-10 12:50:30 +0100

    cors

--- controllers/pharmcontroller.js
+++ controllers/pharmcontroller.js
@@ -2,20 +2,42 @@ const Pharm = require("./../model/PharmModel");
 
 exports.getAllpharmacy = async (req, res, next) => {
   try {
-  } catch (err) {}
+    const pharmacies = await Pharm.find();
+    return res.status(200).json({
+      data: pharmacies,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      err: err,
+      message: "unable to find ",
+    });
+  }
 };
 
 exports.getonepharmacy = async (req, res, next) => {
   try {
-  } catch (err) {}
+    const pharmacies = await Pharm.findById(req.params.id);
+    if (!pharmacies) {
+      return res.status(400).json({
+        message: "no pharmacies found",
+      });
+    }
+    return res.status(200).json({
+      data: pharmacies,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      err: err,
+      message: "unable to find ",
+    });
+  }
 };
 
 exports.createonepharmacy = async (req, res, next) => {
   try {
-    const pharm = await Pharm.create(req.body);
-
+    const pharmacies = await Pharm.create(req.body);
     return res.status(200).json({
-      data: pharm,
+      data: pharmacies,
     });
   } catch (err) {
     return res.status(400).json({
@@ -27,10 +49,28 @@ exports.createonepharmacy = async (req, res, next) => {
 
 exports.updateonepharmacy = async (req, res, next) => {
   try {
-  } catch (err) {}
+    const pharmacies = await Pharm.findByIdAndUpdate(req.params.id, req.body);
+    return res.status(200).json({
+      data: pharmacies,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      err: err,
+      message: "unable to update pharmacy ",
+    });
+  }
 };
 
 exports.deleteonepharmacy = async (req, res, next) => {
   try {
-  } catch (err) {}
+    await Pharm.findByIdAndDelete(req.params.id);
+    return res.status(200).json({
+      message: "pharmacies has been deleted",
+    });
+  } catch (err) {
+    return res.status(400).json({
+      err: err,
+      message: "unable to delete pharmacy ",
+    });
+  }
 };
--- model/PharmModel.js
+++ model/PharmModel.js
@@ -1,5 +1,22 @@
 const mongoose = require("mongoose");
 
-const pharmschema = new mongoose.Schema({});
+const pharmschema = new mongoose.Schema({
+  premisesName: {
+    type: String,
+    required: true,
+  },
+  superintendent: {
+    type: String,
+    required: true,
+  },
+  certificateNumber: {
+    type: String,
+    required: true,
+  },
+  premisesAddress: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model("Pharm", pharmschema);
